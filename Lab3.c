8fdc20144f77c09cd9217492de7ea05bdf8b5789
Matthew Nadolny <mnadolny@email.arizona.edu>
2014-10-30 11:19:14 -0700

    Implimented H-Bridge into hardware and specified pins(SEE TOP OF LAB3.C).  Implimented state machine for Forward/Backwards/Idle button pressing.  Tested and Code works.  Drive function works on robot.  Finished LAB 3.  Code needs cleaned up and commented.

--- Lab3.c
+++ Lab3.c
@@ -3,6 +3,18 @@
 // MACROS for special function registers (SFR) and control bits within those
 // registers.
 
+//pin10 is enable to pin1h and pin16h
+//pin7 goes to pin2h
+//pin6 goes to pin7h
+//right wheel red to pin3h
+//right wheel black to pin6h
+//left wheel red to pin11h
+//left wheel black to pin14h
+//+5v to pin8h and pin9h
+//pin21 goes to pin10h
+//pin22 goes to pin15h
+//ground pin4h,5h,12h,13h
+
 #include "p24fj64ga002.h"
 #include <stdio.h>
 #include "lcd.h"
@@ -27,10 +39,15 @@ _CONFIG1( JTAGEN_OFF & GCP_OFF & GWRP_OFF &
 _CONFIG2( IESO_OFF & SOSCSEL_SOSC & WUTSEL_LEG & FNOSC_PRIPLL & FCKSM_CSDCMD & OSCIOFNC_OFF &
 		 IOL1WAY_OFF & I2C1SEL_PRI & POSCMOD_XT )
 
+                                                                  // forward 1 backwards 0
+    volatile int state = 0;
+    volatile int lastState = 1;
+    volatile int flag = 1;
 
 int main(void){
 
 // ****************************** INITIALIZE ******************************** //
+
     unsigned long int temp;                                                     // Initialize temp variable for calc ADC_value
     int ADC_value;                                                              // Initialize integer to hold average of adcBuff sampling
     char value1[8];                                                             // Initialize string for ADC_value storage
@@ -44,8 +61,12 @@ int main(void){
     
     OC1CON = 0x000E;                                                            // Set OC1CON to PWM w/o protection
     OC2CON = 0x000E;                                                            // Set OC2CON to PWM w/o protection
+    OC3CON = 0x000E;
+    OC5CON = 0x000E;
     OC1R = 0;                                                                   // Initialize duty cycle for OC1R to 0%
     OC2R = 0;                                                                   // Initialize duty cycle for OC2R to 0%
+    OC3R = 0;
+    OC5R = 0;
 
     T3CON = 0x8020;                                                             // Turn on and set prescaler to 64
     TMR3 = 0;
@@ -63,12 +84,21 @@ int main(void){
     AD1CSSL = 0;                                                                // No inputs are scanned
     AD1CON1bits.ADON = 1;                                                       // Turn ADC on
 
-    RPOR1bits.RP2R = 18;
-    RPOR4bits.RP9R = 19;
+//    RPOR1bits.RP2R = 18;                                                        //pin6 used for OC1
+//    RPOR1bits.RP3R = 20;                                                        //pin7 used for OC1 ground
+//
+//    RPOR5bits.RP10R = 19;                                                        //pin21 used for OC2
+//    RPOR5bits.RP11R = 20;                                                        //pin22 used for OC2 ground
 
-    TRISAbits.TRISA3 = 0;                                                       //RB3 = pin 10
-    PORTAbits.RA3 = 0;
+    TRISAbits.TRISA3 = 0;                                                       //RB3 = pin 10 as output
+    TRISBbits.TRISB5 = 1;                                                       //RB5 is input, SW5 on PIC
+    //PORTBbits.RB5 = 1;
 
+    CNEN2bits.CN27IE = 1;                                                        //enable change notification for RB5
+
+
+    IFS1bits.CNIF = 0;                                                          //drop the CN flag
+    IEC1bits.CNIE = 1;                                                          //allow change notifications
 
     ///////////////////////////FIX ME/////////////////////////////////
 //    ADD Change notification for button press idle, forwards, backwards
@@ -77,8 +107,16 @@ int main(void){
 
 
 
-// ******************************* SAMPLING ********************************* //
+
     while(1){
+
+        switch (state){
+
+
+                case 0:                                                         // Pulse State
+
+
+// ******************************* SAMPLING ********************************* //
         while(!IFS0bits.AD1IF);                                                 // While conversion not done
         adcPtr = (unsigned int *)(&ADC1BUF0);                                   // yes
         IFS0bits.AD1IF = 0;                                                     // Clear AD1IF
@@ -86,7 +124,7 @@ int main(void){
         for (i=0;i<16;i++) {                                                    // Iterrate to sum up adcBuff
             adcBuff[i] = *adcPtr++;
             temp = temp + adcBuff[i];   
-    }
+        }
                      
     ADC_value = temp/16;                                                        // Average the 16 ADC value = binary->decimal conversion
 
@@ -111,6 +149,8 @@ int main(void){
     }
 // *************************** CALCULATIONS END ***************************** //
 
+ 
+
 
 
 // ****************************** UPDATE LCD ******************************** //
@@ -126,11 +166,69 @@ int main(void){
     LCDMoveCursor(1,5);                                                         // Move cursor to middle of line 2
     LCDPrintString(value3);                                                     // Print value3 to the lcd
 
-//    sprintf(value, "%6.2f", AD_value);
-//    LCDMoveCursor(1,0);
-//    LCDPrintString(value);                                                      //print digital value to lcd
+                                                 
 // **************************** UPDATE LCD END ****************************** //
 
+             break;
+
+
+
+            case 1:                                                             // Forward State
+
+            PORTAbits.RA3 = 0;                                                  // Turn H-Bridge on
+            RPOR1bits.RP2R = 18;                                                //pin6 used for OC1
+            RPOR1bits.RP3R = 20;                                                //pin7 used for OC1 ground
+
+            RPOR5bits.RP10R = 19;                                               //pin21 used for OC2
+            RPOR5bits.RP11R = 20;                                               //pin22 used for OC2 ground
+            lastState = 3;                                                      // last state was forward
+            flag = 0;
+            state = 0;
+                break;
+
+
+
+            case 2:                                                             // Reverse State
+
+            PORTAbits.RA3 = 0;                                                  // Turn H-Bridge on
+            RPOR1bits.RP2R = 20;                                              //pin6 used for OC1
+            RPOR1bits.RP3R = 18;                                              //pin7 used for OC1 ground
+
+            RPOR5bits.RP10R = 20;                                             //pin21 used for OC2
+            RPOR5bits.RP11R = 19;                                             //pin22 used for OC2 ground
+            lastState = 3;
+            flag = 1;
+            state = 0;
+
+            break;
+
+
+            case 3:                                                             // Idle State
+                OC1RS = 0;
+                OC2RS = 0;
+//            PORTAbits.RA3 = 1;                                                  // Turn H-Bridge off
+           //state = 0;
+            if (flag == 0){
+                lastState = 2;
+            }
+            if (flag == 1){
+                lastState =1;
+            }
+
+            break;
+
+            case 4:                                                             // Debounce
+                if(PORTBbits.RB5 == 1){
+                    state = lastState;
+                }
+                break;
+
+
+
+
+
+
+        }
 
 
 
@@ -139,45 +237,45 @@ int main(void){
     // modify this to use Change Notification Interrupt when written//
     //////////////////////////////////////////////////////////////////
 
-//    switch{
-//        case 0:                                                                 // DEBOUNCE/CHANGE STATE
-//            if (button released){
-//                if (state 1 || state 2){
-//                  go to state3
-//                }
-//
-//                if (state 3 and variable = 0){
-//                  state = 1;
-//                }
-//
-//                if (state 3 and variable = 1){
-//                  state = 2;
-//                }
+//    switch(state)
+//        case 0:                                                                 // DEBOUNCE1
+//            state = 1;
 //            break;
 //
-//        case 1:                                                                 // FORWARD STATE
-//            if {button pressed){                                                // If button press
+//        case 2:                                                                 // FORWARD STATE
+//            state = 0;
+//            RPOR1bits.RP2R = 18;                                           //pin6 used for OC1
+//            RPOR1bits.RP3R = 20;                                           //pin7 used for OC1 ground
+//
+//            RPOR5bits.RP10R = 19;                                            //pin21 used for OC2
+//            RPOR5bits.RP11R = 20;                                           //pin22 used for OC2 ground
+//            lastState = 1;                                                    // last state was forward
+//            break;
+//
+//            case 3:
+//
+//        case 4:                                                                 // REVERSE STATE
+//
 //              state = 0;                                                        // Go to Debounce/Change state
+//                RPOR1bits.RP2R = 20;                                              //pin6 used for OC1
+//                RPOR1bits.RP3R = 18;                                              //pin7 used for OC1 ground
+//
+//                RPOR5bits.RP10R = 20;                                             //pin21 used for OC2
+//                RPOR5bits.RP11R = 19;                                             //pin22 used for OC2 ground
+//               lastState = 0;
 //            }
 //            break;
 //
-//        case 2:                                                                 // REVERSE STATE
-//            if (button pressed){                                                // If button press
-//              state = 0;                                                        // Go to Debounce/Change state
-//            }
-//            break;
-//
-//        case 3:                                                                 // IDLE STATE
-//            if (button pressed){                                                // If button press
-//              state = 0;                                                        // Go to Debounce/Change state
+//        case 5:                                                                 // IDLE STATE
+//                OC1R = 0;                                                                   // Initialize duty cycle for OC1R to 0%
+//                OC2R = 0;
 //            break;
 //
 //    }
 
 
-
-
     }
+    
 
 
 
@@ -194,5 +292,27 @@ void _ISR_ADC1Interrupt(void) {
 
 void __attribute__((interrupt)) _T3Interrupt(void){
     IFS0bits.T3IF = 0;
+
+}
+
+void __attribute__((interrupt)) _CNInterrupt(void){
+    IFS1bits.CNIF = 0;
+    state = 4;
+
+//    if(state == 3 && PORTBbits.RB5 == 1 && lastState == 1)
+//    {
+//        state = 2;
+//    }
+//
+//     if(state == 3 && PORTBbits.RB5 == 1 && lastState == 0)
+//    {
+//        state = 0;
+//
+//    }
+//
+//    if(state == 1 && PORTBbits.RB5 == 1)
+//    {
+//        state = 0;
+//    }
 
 }
